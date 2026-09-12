ae887de6b655bfd8b0d078d96c444dd75d1051b1
Ashutoshtiwari902 <ashutoshtiwari.9at@gmail.com>
2026-09-12 23:53:30 +0530

    Initial commit

---
+++ week2/src/Week1/week1Assignmant.java
@@ -0,0 +1,37 @@
+package Week1;
+
+public class week1Assignmant {
+    static void checkTypingAccuracy(String original, String typed) {
+
+        int matched = 0;
+        int firstMismatch = -1;
+
+        for (int i = 0; i < original.length(); i++) {
+
+            if (original.charAt(i) == typed.charAt(i)) {
+                matched++;
+            } else if (firstMismatch == -1) {
+                firstMismatch = i;
+            }
+        }
+
+        double accuracy = (matched * 100.0) / original.length();
+
+        System.out.printf("Matched: %d/%d | Accuracy: %.2f%%",
+                matched, original.length(), accuracy);
+
+        if (firstMismatch != -1) {
+            System.out.println(" | First Mismatch at position "
+                    + (firstMismatch + 1)
+                    + " ('" + original.charAt(firstMismatch)
+                    + "' vs '" + typed.charAt(firstMismatch) + "')");
+        } else {
+            System.out.println(" | No Mismatches");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        checkTypingAccuracy("hello world", "hello wolt");
+    }
+}
